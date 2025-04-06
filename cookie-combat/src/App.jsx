25072e64a75aa08f752fd428e46bcc028e83642a
ValeryManoilov <manojlovv@inbox.ru>
2025-04-06 20:14:13 +0300

    add data

--- cookie-combat/src/App.jsx
+++ cookie-combat/src/App.jsx
@@ -1,34 +1,102 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useEffect, useState } from 'react';
+import './App.css';
+import styled from "styled-components";
+import axios from "axios";
+
+const IncrementButton =  styled.button`
+
+`
+
+const ClickerContainer =  styled.div`
+
+`
+
+const ClickerContent = styled.div`
+
+`
+
+const Scores = styled.div`
+
+`
+
+
 
 function App() {
-  const [count, setCount] = useState(0)
 
+  const [scores, setScores] = useState();
+  const tg = window.Telegram.WebApp;
+
+  function GetScoresByTelegramId()
+  {
+            
+    tg.ready();
+    tg.expand();
+
+    if (tg.initDataUnsafe?.user) {
+      const user = tg.initDataUnsafe.user;
+      axios.get("https://localhost:7057/checkuser", {
+          params:
+          {
+              telegramId: user.id
+          },
+          headers: 
+          {
+              "Content-type": "application/json"
+          }
+      })
+      .then((res) => {
+        setScores(res.data)
+      })
+      .catch((err) => {
+        console.log(err)
+      })
+    }
+    else {
+        console.error('User data not available');
+    }
+  }
+
+  function AddScoresAsync()
+  {
+    tg.ready();
+    tg.expand();
+
+    if (tg.initDataUnsafe?.user) {
+      const user = tg.initDataUnsafe.user;
+      axios.post("https://localhost:7057/addscores", 
+          {
+            telegramId: user.id
+          },
+          {
+            headers: 
+            {
+                "Content-type": "application/json"
+            }
+          }
+      )
+      .then((res) => {
+        setScores(res.data)
+      })
+      .catch((err) => {
+        console.log(err)
+      })
+    }
+    else {
+        console.error('User data not available');
+      }
+    }
+
+  useEffect(() => {
+    GetScoresByTelegramId()
+  }, [])
+  
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+    <ClickerContainer>
+      <ClickerContent>
+        <Scores>{scores}</Scores>
+        <IncrementButton onClick={() => AddScoresAsync()}>+</IncrementButton>
+      </ClickerContent>
+    </ClickerContainer>
   )
 }
 
--- cookie-combat/src/main.jsx
+++ cookie-combat/src/main.jsx
@@ -3,6 +3,16 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
 
+document.addEventListener('DOMContentLoaded', () => {
+  if (window.Telegram?.WebApp) {
+    window.Telegram.WebApp.ready();
+    window.Telegram.WebApp.expand();
+  } else {
+    console.error('Telegram WebApp SDK not loaded');
+  }
+});
+
+
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <App />
